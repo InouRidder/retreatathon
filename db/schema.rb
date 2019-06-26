ae8d72ec60c7fd176a244e3d09d54e0f89babf5b
Gianluca Perrone <perronegian@hotmail.com>
2019-06-26 10:23:47 -0300

    FEAT: Generated tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190626131524) do
+ActiveRecord::Schema.define(version: 20190626132239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,9 @@ ActiveRecord::Schema.define(version: 20190626131524) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.bigint "product_id"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_tags_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,5 +47,4 @@ ActiveRecord::Schema.define(version: 20190626131524) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "tags", "products"
 end
