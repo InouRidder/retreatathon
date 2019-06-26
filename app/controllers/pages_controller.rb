d5aee634a48d645f2c1c1fc1c3a9376dbedeb220
cedricmenteau <me.cedric@gmail.com>
2019-06-26 18:20:26 +0200

    Move logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,18 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    products = Product.all
-    @products = Kaminari.paginate_array(products).page(params[:post_page]).per(12)
+    if query_params
+      @products = QueryProducts.new(query_params)
+      @products = Kaminari.paginate_array(@products).page(params[:post_page]).per(12)
+    else
+      @products = Product.all
+      @products = Kaminari.paginate_array(@products).page(params[:post_page]).per(12)
+    end
+  end
+
+  private
+
+  def query_params
+    @parameters ||= params[:query]
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,3 @@
 class ProductsController < ApplicationController
-  def index
-    if query_params
-      @products = QueryProducts.new(query_params)
-      @products = Kaminari.paginate_array(products).page(params[:post_page]).per(12)
-    else
-      @products = Product.all
-      @products = Kaminari.paginate_array(products).page(params[:post_page]).per(12)
-    end
-  end
-
-  private
-
-  def query_params
-    @parameters ||= params[:query]
-  end
+ 
 end
