236298f25e712a6783b6d8ab519216cd77dcb4c4
Inou Ridder <inouridder@gmail.com>
2019-06-26 15:17:35 +0200

    FEAT: Add gems attribute to products as array

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190626105011) do
+ActiveRecord::Schema.define(version: 20190626131524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20190626105011) do
     t.string "github_repository_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "gems", default: [], array: true
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_tags_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +48,5 @@ ActiveRecord::Schema.define(version: 20190626105011) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tags", "products"
 end
