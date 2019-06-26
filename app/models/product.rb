8ebeb7500690832edac6a8b4be5e1b7869b77baf
Gianluca Perrone <perronegian@hotmail.com>
2019-06-26 08:33:09 -0300

    Searchable product, but not though associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  include PgSearch
+  pg_search_scope :global_search,
+    against:[  
+						 :name,
+						 :customer_segment,
+						 :tagline,
+						 :pain,
+						 :solution,
+						 :user_core_journey,
+						 :originality,
+						],
+		associated_against: {},
+    using: {
+      tsearch: { prefix: true }
+    }
 end
