7d424172e86147a1d4641044f281eca9c32937b2
Gabriel <dehan.gabriel@gmail.com>
2019-06-26 15:42:22 +0200

    Importing the CSV of doom

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190626105011) do
+ActiveRecord::Schema.define(version: 20190626132657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_members_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -26,6 +35,8 @@ ActiveRecord::Schema.define(version: 20190626105011) do
     t.string "github_repository_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "host"
+    t.string "batch"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +51,5 @@ ActiveRecord::Schema.define(version: 20190626105011) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "members", "products"
 end
