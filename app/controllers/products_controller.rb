341d7d468d17cfe0c7689b49019e498f7692cbd4
Inou Ridder <inouridder@gmail.com>
2019-06-27 13:24:29 +0200

    FIX: Move from pages#home to products#index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
+  def index
+    @number_of_products = Product.all.length
+    if query_params
+      @products = QueryProducts.new({ params: query_params }).call
+    else
+      @products = Product.all
+    end
+    @products = Kaminari.paginate_array(@products).page(params[:page]).per(12)
+  end
+
   def autocomplete
     render json: {
       products: QueryProducts.new({ params: { queries: [params[:q]] } }).call.map(&:name)
@@ -8,5 +18,12 @@ class ProductsController < ApplicationController
   end
 
   private
+
+
+  private
+
+  def query_params
+    params.permit(:queries, :tags)
+  end
 
 end
